3bb86e9ba8390a65df8362d4b0be5a64331f05f1
Imato <alexey.varentsov@gmail.com>
2014-09-01 07:04:06 +0400

    2

---
+++ RS_Dash_Test/RS_Dash_Test/RsAdapterTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RS_dash.Utils;
+using RS_dash.Model;
+using System.Collections.Generic;
+
+namespace RS_Dash_Test
+{
+    [TestClass]
+    public class RsAdapterTest
+    {
+        RsAdapter rsAdapter;
+
+        [TestInitialize]
+        public void initTest()
+        {
+            rsAdapter = RsAdapter.getAdapter("rusprsql001", "moneks", "avarentsov", "leto@14");
+
+        }
+
+        [TestMethod]
+        public void checkAdapter()
+        {
+            Assert.IsNotNull(rsAdapter);
+        }
+
+        [TestMethod]
+        public void getReports()
+        {
+            rsAdapter = RsAdapter.getAdapter("rusprsql001", "moneks", "avarentsov", "leto@14");
+
+            List<Report> reports = rsAdapter.getReports("RD-010");
+            if (reports != null)
+            {
+                foreach (var r in reports)
+                {
+                    Console.WriteLine(r.Path + " - " + r.Id);
+                }
+            }
+
+            Assert.IsNull(reports);
+
+        }
+
+
+    }
+}
